fd5d427c1db1c8911d3532456fdf088fc509767c
shrutigupta263 <shrutiguptaab263@gmail.com>
2022-12-02 00:15:37 +0530

    codding

---
+++ voidpointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a = 35;
+    float b = 8.3;
+    void *ptr;
+    ptr = &b;
+    printf("The value of b is %f\n", *((float *)ptr));
+    ptr = &a;
+    printf("The value of a is %d\n", *((int *)ptr));
+    ptr = &a;
+    printf ("The value of a is %d\n", *((int *)ptr));
+    ptr = &a;
+    printf ("The value of a is %d\n", *((int *)ptr));
+    return 0;
+}
